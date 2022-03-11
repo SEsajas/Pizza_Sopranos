704dd2b30bf8423adce78728d7ee7015c4b2c3a8
Sanne Esajas <302560545@student.rocmondriaan.nl>
2022-03-11 12:34:31 +0100

    new templates

---
+++ pizza site/src/Controller/PizzaController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PizzaController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage(): Response {
+        $categories = [
+            'Vlees',
+            'Vegetarisch',
+            'Vis'
+        ];
+        return $this->render('pizza/categories.html.twig', [
+            'categories' => $categories
+        ]);
+    }
+    /**
+     * @Route("/{cat}")
+     */
+    public function category($cat){
+
+        $pizza = [
+            'Vlees',
+            'Vegetarisch',
+            'Vis'
+        ];
+        return $this->render('pizza/category',[
+            'cat'=> $cat,
+            'pizzas' => $pizza
+        ]);
+    }
+
+}
